e0e416fe2fa2ec19a2a4d198adbadac42efe459c
kmx98 <mariokmxfx@gmail.com>
2025-07-28 15:45:42 -0700

    Primer commit

---
+++ cartmarius-web/src/main/java/com/ecommerce/cartmarius/cartmarius_web/CartmariusWebApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.cartmarius.cartmarius_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CartmariusWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CartmariusWebApplication.class, args);
+	}
+
+}
---
+++ cartmarius-web/src/test/java/com/ecommerce/cartmarius/cartmarius_web/CartmariusWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.cartmarius.cartmarius_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CartmariusWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
