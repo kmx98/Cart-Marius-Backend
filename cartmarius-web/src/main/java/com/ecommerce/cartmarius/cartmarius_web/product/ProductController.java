fdba62403bad73b62bb9295b1b37fd3ae5f5ca23
kmx98 <mariokmxfx@gmail.com>
2025-08-04 16:14:08 -0700

    Conexion entre Frontend y Backend

--- cartmarius-web/src/main/java/com/ecommerce/cartmarius/cartmarius_web/product/ProductController.java
+++ cartmarius-web/src/main/java/com/ecommerce/cartmarius/cartmarius_web/product/ProductController.java
@@ -4,6 +4,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +17,7 @@ import com.ecommerce.cartmarius.cartmarius_web.product.dto.ProductDetailsDTO;
 
 @RestController
 @RequestMapping("/games")
+@CrossOrigin(origins = "http://localhost:4200") // Permite que Angular acceda a esta API
 public class ProductController {
 
     private GameRepository repository;
